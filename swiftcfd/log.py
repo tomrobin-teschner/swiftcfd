c77da1f063139b24ba528d73d57d4ec574f9d43b
tomrobin-teschner <tomrobin.teschner@yahoo.de>
2026-01-13 00:24:43 +0900

    updated logging class
    now the logging class will print all information per time step at once.

--- swiftcfd/log.py
+++ swiftcfd/log.py
@@ -2,13 +2,16 @@ class Log:
     def __init__(self):
         pass
 
-    def print_time_step(self, time, equation):
+    def print_time_step(self, time, equations):
         current_time = time.current_time
         cfl = time.CFL
         dt = time.dt
-        is_diagonal, num_iterations, res_norm, has_converged = equation.solver.get_solver_statistics()
 
-        if is_diagonal:
-            print(f'Time: {time.current_time:.2e}, dt: {dt:.1f}, CFL: {cfl:.2f}')
-        else:
-            print(f'Time: {time.current_time:.2e}, dt: {dt:.1f}, CFL: {cfl:.2f}, iterations: {num_iterations:<5}, res_norm: {res_norm:.3e}, has_converged: {has_converged}')
+        print(f'Time step: {time.timestep + 1:<5}, Current time: {time.current_time:.2e}, CFL: {cfl:.2e}, dt: {dt:.2e}')
+
+        for equation in equations:
+            is_diagonal, num_iterations, res_norm, has_converged = equation.solver.get_solver_statistics()
+
+            if not is_diagonal:
+                print(f'{equation.get_variable_name()}: iterations: {num_iterations:<5}, Residual: {res_norm:.3e}, Converged?: {has_converged}')
+        print()
