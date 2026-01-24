3f7171c80aaf05167c271ff4263aa4bd369e0975
tomrobin-teschner <tomrobin.teschner@yahoo.de>
2026-01-24 23:36:55 +0900

    bugfix: corrected coefficients in second order backward scheme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # swiftcfd
 
-![Static Badge](https://img.shields.io/badge/Version-0.25.0-blue)
+![Static Badge](https://img.shields.io/badge/Version-0.25.1-blue)
 
 > [!WARNING]  
 > This project is work in progress and in version 0.X, expect breaking changes with new features.
--- swiftcfd/equations/numericalSchemes/implicit/secondOrderBackwards.py
+++ swiftcfd/equations/numericalSchemes/implicit/secondOrderBackwards.py
@@ -7,10 +7,10 @@ class SecondOrderBackwards(NumericalSchemesBase):
     def _compute_coefficients(self, direction, runtime, var_name, multiplier):
         dt = runtime.dt
         dt_old = runtime.old_dt
-        if runtime.timestep > 0:
+        if runtime.timestep > 1:
             for block_id in range(0, self.mesh.num_blocks):
                 self.coefficients.append({
-                    'ap': 1.0/(dt + dt_old) * multiplier,
+                    'ap': 3.0/(dt + dt_old) * multiplier,
                     'b':  1.0/(dt + dt_old) * multiplier,
                     'is_second_order': True
                 })
@@ -25,11 +25,21 @@ class SecondOrderBackwards(NumericalSchemesBase):
     def _compute_interior(self, direction, block_id, solver, var_name):
         for (i, j) in self.mesh.internal_loop_single_block(block_id):
             ap_index = self.mesh.map3Dto1D(block_id, i, j)
-            b_index  = self.mesh.map3Dto1D(block_id, i, j)
-
             solver.add_to_A(ap_index, ap_index, self.coefficients[block_id]['ap'])
-            solver.add_to_b(b_index, self.coefficients[block_id]['b'] * self.field_manager.fields[var_name].old[block_id, i, j])
-    
+
+        inv_2dt = self.coefficients[block_id]['b']
+        if self.coefficients[block_id]['is_second_order']:
+            for (i, j) in self.mesh.internal_loop_single_block(block_id):
+                b_index = self.mesh.map3Dto1D(block_id, i, j)
+                b_value = 4.0 * inv_2dt * self.field_manager.fields[var_name].old[block_id, i, j] - \
+                    inv_2dt * self.field_manager.fields[var_name].oldold[block_id, i, j]
+                solver.add_to_b(b_index, b_value)
+        else:
+            for (i, j) in self.mesh.internal_loop_single_block(block_id):
+                b_index = self.mesh.map3Dto1D(block_id, i, j)
+                b_value = inv_2dt * self.field_manager.fields[var_name].old[block_id, i, j]
+                solver.add_to_b(b_index, b_value)
+
     def get_right_hand_side_contribution(self, direction, ij, ip1j, im1j, ijp1, ijm1, var_name):
         block1, i1, j1 = ij
         multiplier = self.coefficients[block1]['b']
